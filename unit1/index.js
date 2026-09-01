b7f3a2e92d97c3a10675c1f93c10aefb3373c20f
Akhilesh yadav <akhileshyadav310596@gmail.com>
2026-09-01 11:09:31 +0530

    update

---
+++ unit1/index.js
@@ -0,0 +1,12 @@
+function calcAge(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge(1999);
+console.log(age1);
+
+ const age = function calcAgge1(birthYear){
+    return 2037 - birthYear;
+}
+
+console.log(age(1999));
